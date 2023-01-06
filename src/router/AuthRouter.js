1b5373093b06ac09145d0c6b2acd7f032417e431
Oscar Acuña <oscagustavo1961@gmail.com>
2023-01-06 13:47:53 -0600

    Cambios Nuevos

--- src/router/AuthRouter.js
+++ src/router/AuthRouter.js
@@ -1,9 +1,10 @@
 // Ruta SECUNDARIA de la Aplicación
 import React from 'react'
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { LoginPage, RegisterPage } from '../pages'
 /*Importar Estilos CSS */
 import '../css/login-register.css'
+import { NotFound404 } from '../pages/NotFound404'
 
 export const AuthRouter = () => {
     return (
@@ -11,9 +12,10 @@ export const AuthRouter = () => {
             <div className="container-login100">
                 <div className="wrap-login100 p-t-50 p-b-90 fw-bold">
                     <Routes>
-                        <Route path="login" element={<LoginPage />} />
+                        <Route path="/" element={<LoginPage />} />
                         <Route path="register" element={<RegisterPage />} />
-                        <Route path="/*" element={<Navigate to="login" />} />
+                        {/* <Route path="/*" element={<Navigate to="login" />} /> */}
+                        <Route path="*" element={<NotFound404 />} /> 
                     </Routes>
                 </div>
             </div>
